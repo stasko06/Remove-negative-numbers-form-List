a47a46d771456a6977d7a5c7ba837b50ff39afff
stasko06 <shketievstanislav@gmail.com>
2023-04-25 09:46:07 +0300

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,23 @@
+﻿string input = Console.ReadLine();
+List<string> numbers = input.Split(' ').ToList();
+List<int> Numbers = new List<int>();
+for (int i = 0; i<numbers.Count(); i++)
+{
+    Numbers.Add(int.Parse(numbers[i]));
+}
+for (int i = 0; i<Numbers.Count(); i++)
+{
+    if (Numbers[i]<0)
+    {
+        Numbers.RemoveAt(i);
+        i--;//Индекса се връща назад защото при премахване на елемент, следващия се премества с 1 по наляво
+    }
+}
+Numbers.Reverse();
+string output = " ";
+for (int i = 0; i<Numbers.Count(); i++)
+{
+    output+= Numbers[i]+ " ";
+}
+Console.WriteLine(output);
+ 
